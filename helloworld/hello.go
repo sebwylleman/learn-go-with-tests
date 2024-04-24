83a2ea7d4c8ac871a56a48bd56936f1906e3cb57
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-04-24 11:15:02 +0100

    feature: greets in Spanish

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,15 +2,20 @@ package main
 
 import "fmt"
 
+const language = "Spanish"
 const englishHelloPrefix = "Hello, "
+const SpanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == language {
+		return SpanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
