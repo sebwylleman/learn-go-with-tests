7a3a0cd9947a64a28c3c7fade4049d8ce5f82286
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-04-24 11:41:10 +0100

    refactor: greeting prefixes abstracted in function. And no more use of global variables.

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -1,23 +1,31 @@
 package main
 
-import "fmt"
-
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+import (
+	"fmt"
+)
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	const (
+		englishHelloPrefix = "Hello, "
+		spanishHelloPrefix = "Hola, "
+		frenchHelloPrefix  = "Bonjour, "
+	)
 	switch language {
 	case "French":
 		prefix = frenchHelloPrefix
 	case "Spanish":
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix + name
+	return prefix
 }
 
 func main() {
