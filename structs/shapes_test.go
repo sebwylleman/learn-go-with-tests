91b8a5686c28058cb30ed0522a0972fd542971da
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-04-26 08:22:05 +0100

    table driven test with new Triangle shape added

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,22 +13,18 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f, want %.2f", got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g, want %g", got, tt.want)
 		}
 	}
-	t.Run("recatangles", func(t *testing.T) {
-		r := Rectangle{12.0, 6.0}
-		want := 72.0
-		checkArea(t, r, want)
-	})
-	t.Run("circles", func(t *testing.T) {
-		c := Circle{10}
-		want := 314.1592653589793
-		checkArea(t, c, want)
-	})
 }
