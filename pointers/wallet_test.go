489696e4ffec21c9487dd98101f4d33b19e21da0
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-04-26 10:37:44 +0100

    Withdraw method

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,13 +5,25 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s, want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(10)}
+		wallet.Withdraw(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(0)
+
+		if got != want {
+			t.Errorf("want %s, got %s", got, want)
+		}
+	})
 }
