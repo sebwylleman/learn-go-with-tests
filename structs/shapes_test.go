b952e74641a42751c7421fa1055cfbb324bffe78
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-04-26 08:01:28 +0100

    refactor: using a Shape interface

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,24 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("recatangles", func(t *testing.T) {
-		r := Rectangle{12.0, 6.0}
 
-		got := r.Area()
-		want := 72.0
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %.2f, want %.2f", got, want)
 		}
+	}
+	t.Run("recatangles", func(t *testing.T) {
+		r := Rectangle{12.0, 6.0}
+		want := 72.0
+		checkArea(t, r, want)
 	})
 	t.Run("circles", func(t *testing.T) {
 		c := Circle{10}
-
-		got := c.Area()
 		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
-		}
+		checkArea(t, c, want)
 	})
 }
