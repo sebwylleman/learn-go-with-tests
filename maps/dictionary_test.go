72054484f0b64d4fa9e11f2848ef43989d2a839e
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-04-28 07:47:01 +0100

    refactor: assertStrings helper function

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -6,8 +6,12 @@ func TestSearch(t *testing.T) {
 	dictionary := map[string]string{"test": "this is just a test"}
 	got := Search(dictionary, "test")
 	want := "this is just a test"
+	assertStrings(t, got, want)
+}
 
-	if want != got {
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
 }
