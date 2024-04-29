7af6495f673fe5f21d603bc2e338f64a4c2e4741
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-04-29 06:20:54 +0100

    constraint: only adds a new word if it does not exist

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -20,16 +20,34 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
-	want := "this is just a test"
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	got, err := dictionary.Search("test")
+		err := dictionary.Add(word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, definition, word)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, definition)
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, definition, word)
+	})
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, definition, word string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
 	if err != nil {
-		t.Fatal("should find added word,", err)
+		t.Fatal("Should find added word,", err)
 	}
-
-	assertStrings(t, want, got)
+	assertStrings(t, got, definition)
 }
 
 func assertStrings(t testing.TB, got, want string) {
